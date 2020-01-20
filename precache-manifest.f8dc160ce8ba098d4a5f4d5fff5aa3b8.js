6304e5367d96a1112f8ba7264996682f0fb6ae7c
Hikaru-L <hikaru.outsmart@gmail.com>
2020-01-20 12:28:46 -0200

    Updates

---
+++ precache-manifest.f8dc160ce8ba098d4a5f4d5fff5aa3b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a925f58f1bd87c16f4e5515995b08a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "916faead0ae83d3bc9f2",
+    "url": "/static/css/main.6b5c7cee.chunk.css"
+  },
+  {
+    "revision": "39747afbc867668358c6",
+    "url": "/static/js/2.523a4654.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.523a4654.chunk.js.LICENSE"
+  },
+  {
+    "revision": "916faead0ae83d3bc9f2",
+    "url": "/static/js/main.8ce4d6a2.chunk.js"
+  },
+  {
+    "revision": "555e721016a200981410",
+    "url": "/static/js/runtime-main.f0a4ed94.js"
+  },
+  {
+    "revision": "47986a85fa3ada78d7ac2af7d160550d",
+    "url": "/static/media/aaamatlogo.47986a85.png"
+  },
+  {
+    "revision": "1cc1dde99e108bd828a12ce8b4aa6616",
+    "url": "/static/media/outsmartlogo.1cc1dde9.png"
+  },
+  {
+    "revision": "b225f49a8abb207591300a20c6ef77e4",
+    "url": "/static/media/testprofilepic.b225f49a.jpg"
+  }
+]);
